5fa21a27c6934f2c1382a5f720d03f78bf2c67eb
Julius Cinco Cesar <knznsmn@icloud.com>
2024-10-09 22:57:46 +0400

    Add project [drum]

---
+++ projets/drum/main.js
@@ -0,0 +1,44 @@
+// Element Bindings
+const keys = {
+  q: document.getElementById('q'),
+  w: document.getElementById('w'),
+  e: document.getElementById('e'),
+  a: document.getElementById('a'),
+  s: document.getElementById('s'),
+  d: document.getElementById('d'),
+  j: document.getElementById('j'),
+  k: document.getElementById('k'),
+  l: document.getElementById('l'),
+}
+
+const sounds = {
+  q: new Audio('./public/wav/boom.wav'),
+  w: new Audio('./public/wav/clap.wav'),
+  e: new Audio('./public/wav/hiha.wav'),
+  a: new Audio('./public/wav/kick.wav'),
+  s: new Audio('./public/wav/nare.wav'),
+  d: new Audio('./public/wav/open.wav'),
+  j: new Audio('./public/wav/ride.wav'),
+  k: new Audio('./public/wav/tink.wav'),
+  l: new Audio('./public/wav/tome.wav'),
+}
+
+// Function Hall
+function playSound(e) {
+  const sound = sounds[e];
+  sound.currentTime = 0;
+  sounds[e].play();
+}
+function playAnimation(e) {
+
+}
+// MAIN()
+
+// Listeners
+document.addEventListener('keydown', (e) => {
+  playSound(e.key)
+});
+
+document.addEventListener('click', (e) => {
+  playSound(e.target.id);
+})
