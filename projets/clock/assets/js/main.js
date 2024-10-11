a7bb691c3733505341dd16c52123a4e6a4824da0
Julius Cinco Cesar <157534723+knznsmn@users.noreply.github.com>
2024-10-11 21:52:46 +0400

    Update clock logic

--- projets/clock/assets/js/main.js
+++ projets/clock/assets/js/main.js
@@ -1,3 +1,9 @@
+// Bindings
+const handHour = document.querySelector('.hour'),
+      handMins = document.querySelector('.minute'),
+      handSecs = document.querySelector('.second');
+    const now = new Date();
+			
 // UI Components
 function Footer() {
 	const year = new Date().getFullYear();
@@ -9,20 +15,21 @@ function Footer() {
 
 // Processes
 function timeKeeper() {
-    const hands = {
-        hour: document.querySelector('.hour'),
-        mins: document.querySelector('.minute'),
-        secs: document.querySelector('.seconds'),
-    }
-    const now = new Date();
-    const time = {
+	const now = new Date();
+	const time = {
         hour: now.getHours(),
         mins: now.getMinutes(),
         secs: now.getSeconds(),
-    }
-    
-    hands[secs].style.rotation = 
+	}
+	console.log(time.secs);
+	const degSecs = `rotate(${time.secs / 60 * 360}deg)`;
+	const degMins = `rotate(${time.mins / 60 * 360}deg)`;
+	const degHour = `rotate(${time.hour / 12 * 360}deg)`;
+	handSecs.style.transform = degSecs;
+	handMins.style.transform = degMins;
+	handHour.style.transform = degHour;
 }
 
 // MAIN()
 Footer();
+setInterval(timeKeeper, 1000);
