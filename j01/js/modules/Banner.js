7448d1d71a85e007d13d6b611179f2741c40bdfc
Julius Cinco Cesar <knznsmn@knznsmn.com>
2025-04-10 16:12:33 +0400

    Add index

--- j01/js/modules/Banner.js
+++
@@ -1,8 +0,0 @@
-import { main } from "./Main.js";
-export function Banner() {
-
-  const drum = document.createElement('img');
-  drum.src = './assets/icons/logo.png';
-  drum.alt = 'Drum Kit';
-  main.appendChild(drum);
-}
--- j01/js/modules/Main.js
+++
@@ -1,1 +0,0 @@
-export const main = document.getElementsByTagName('main')[0];
